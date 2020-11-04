46e566fe43818c7a7331167ce01f16d7961161bd
hampusedeland <hamsteylv@hotmail.com>
2020-11-04 16:03:39 +0100

    Hello johan you are fine

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Den här har ändrats!");
+        System.out.println("HEj johanhandboll!");
     }
 }
