12472e5901044f7b86e3959638e625364fa9c6f0
JohanBlickhammar <johbli@student.chalmers.se>
2020-11-04 16:33:34 +0100

    Mamma

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hej hamguzz!!!!");
-    System.out.println("ylvis")
+    System.out.println("ylvis");
 
     }
 }
