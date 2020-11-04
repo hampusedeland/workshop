abfb39ae68a73a605afe511d88c6ea7933354f72
JohanBlickhammar <johbli@student.chalmers.se>
2020-11-04 16:41:07 +0100

    Achtung

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hej hamguzz!!!!");
     System.out.println("ylvis");
+    System.out.println("curve master Johan");
 
     }
 }
